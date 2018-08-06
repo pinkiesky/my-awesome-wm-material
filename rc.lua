16d2ae9ea08cbbfbb5b79d05f2ffca344a3f76c7
rglr <spore_09@mail.ru>
2018-08-06 11:47:47 +0900

    Removed autostart and unused layouts

--- rc.lua
+++ rc.lua
@@ -56,15 +56,10 @@ editor_cmd = terminal .. " -e " .. editor
 -- However, you can use another modifier like Mod1, but it may interact with others.
 modkey = "Mod4"
 
--- Autostart
-awful.spawn("np-applet")
-
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
     awful.layout.suit.tile,
-    awful.layout.suit.tile.left,
     awful.layout.suit.tile.bottom,
-    awful.layout.suit.tile.top,
     awful.layout.suit.fair,
     awful.layout.suit.fair.horizontal,
     awful.layout.suit.magnifier,
